13608d0f3bcdacf4b7ea2729a41551eb449759cc
mihayosigma <misha042011@gmail.com>
2025-07-15 20:21:43 +0300

    чиним

---
+++ SpaceStrategyProj/Assets/Scripts/ShipController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public ShipModel ship = new ShipModel(1,1,1,1,1,1,1,1,1,1,"a");
+
+    public void Move(Vector3 target, int distance, bool warp)
+    {
+
+    }
+
+    public void Fire(ShipModel targetship)
+    {
+
+    }
+    public void EndTurn()
+    {
+
+    }
+}
---
+++ SpaceStrategyProj/Assets/Scripts/Ships/ShipModel.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipModel
+{
+    public int energy;
+    public int energyregen;
+    public int shield;
+    public int shieldmax;
+    public int shieldregen;
+
+    public int speed;
+    public int warprange;
+
+    public int firepowermin;
+    public int firepowermax;
+    public int firerange;
+
+    public string playername;
+
+    public ShipModel(int energy, int energyregen, int shield, int shieldmax,
+        int shieldregen, int speed, int warprange, int firepowermin, int firepowermax,
+        int firerange, string playername)
+    {
+        this.energy = energy;
+        this.energyregen = energyregen;
+        this.shield = shield;
+        this.shieldmax = shieldmax;
+        this.shieldregen = shieldregen;
+        this.speed = speed;
+        this.warprange = warprange;
+        this.firepowermin = firepowermin;
+        this.firepowermax = firepowermax;
+        this.firerange = firerange;
+        this.playername = playername;
+    }
+
+    public void Regenerate()
+    {
+
+    }
+
+    public int CalculateMoveCost(int distance, bool warp)
+    {
+        return 0;
+    }
+
+    public int CalculateFireCost(int firemin, int firemax)
+    {
+        return firemax - firemin;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+
+    }
+}
