28272c65fa6ee599b0598864c2ef14a862b0c812
mihayosigma <misha042011@gmail.com>
2025-07-31 17:00:04 +0300

    сделал корабли по командам
    нужно нажать на кнопку чтобы передать ход и при повторном нажатии корабли начинают двигаться

--- SpaceStrategyProj/Assets/Scripts/ControllerController.cs
+++ SpaceStrategyProj/Assets/Scripts/ControllerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using static UnityEngine.GraphicsBuffer;
@@ -7,51 +8,84 @@ using static UnityEngine.GraphicsBuffer;
 
 public class ControllerController : MonoBehaviour
 {
+    public List<ShipController> player1shipControllers;
+    public List<ShipController> player2shipControllers;
     public List<ShipController> shipControllers;
     public InputFieldReader input;
     public Button switchshipButton;
     public Button startbutton;
     ShipController currentcontroller;
-    Queue<ShipController> shipQueue = new Queue<ShipController>();
+    Queue<ShipController> player1shipQueue = new Queue<ShipController>();
+    Queue<ShipController> player2shipQueue = new Queue<ShipController>();
     public bool CanMoveShips = false;
+    public bool IsPlayer1Turn = true;
     public void QueueMover()
     {
-        if (shipQueue.Count == 0)
+        if (IsPlayer1Turn)
         {
-            foreach (ShipController controller in shipControllers)
+            if (player1shipQueue.Count == 0)
             {
-                shipQueue.Enqueue(controller);
+                foreach (ShipController controller in player1shipControllers)
+                {
+                    player1shipQueue.Enqueue(controller);
+                }
             }
+            currentcontroller.GetTargetLocation(input.ReadInput());
+            currentcontroller = player1shipQueue.Dequeue();
         }
-        currentcontroller.GetTargetLocation(input.ReadInput());
-        currentcontroller = shipQueue.Dequeue();
+        else
+        {
+            if (player2shipQueue.Count == 0)
+            {
+                foreach (ShipController controller in player2shipControllers)
+                {
+                    player2shipQueue.Enqueue(controller);
+                }
+            }
+            currentcontroller.GetTargetLocation(input.ReadInput());
+            currentcontroller = player2shipQueue.Dequeue();
+        }
     }
 
-        // Start is called before the first frame update
+// Start is called before the first frame update
     void Start()
     {
         switchshipButton.onClick.AddListener(QueueMover);
         startbutton.onClick.AddListener(Watasigma);
-        if (shipQueue.Count == 0)
+        if (player1shipQueue.Count == 0)
         {
-            foreach (ShipController controller in shipControllers)
+            foreach (ShipController controller in player1shipControllers)
             {
-                shipQueue.Enqueue(controller);
+                player1shipQueue.Enqueue(controller);
             }
         }
-        currentcontroller = shipQueue.Dequeue();
-
+        currentcontroller = player1shipQueue.Dequeue();
+        if (player2shipQueue.Count == 0)
+        {
+            foreach (ShipController controller in player2shipControllers)
+            {
+                player2shipQueue.Enqueue(controller);
+            }
+        }
     }
 
     public void Watasigma()
     {
-        if (CanMoveShips)
+        if (IsPlayer1Turn && player1shipQueue.Count == 0)
         {
-            CanMoveShips = false;
+            IsPlayer1Turn = false;
         }
-        else 
+        else
         {
-            CanMoveShips = true;
+            if (CanMoveShips)
+            {
+                CanMoveShips = false;
+            }
+            else
+            {
+                CanMoveShips = true;
+            }
+            IsPlayer1Turn = true;
         }
     }
 
